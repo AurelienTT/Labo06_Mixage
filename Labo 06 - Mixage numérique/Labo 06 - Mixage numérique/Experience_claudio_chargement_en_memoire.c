74cc0aa1301daa32410e286dd260c817e4632b3e
lau1159 <claudio.pintoalmeida@gmail.com>
2016-04-21 17:35:16 +0200

    lol j'ai ajouté un fichier

---
+++ Labo 06 - Mixage numérique/Labo 06 - Mixage numérique/Experience_claudio_chargement_en_memoire.c
@@ -0,0 +1,29 @@
+/* Fichier : Nom_Du_Fichier.c
+* Auteur  : Vous
+* Date    : JJ.MM.AAAA
+*
+* Description : Description de ce que fait le code de ce fichier
+*
+*
+* Remarque(s) :
+*
+* Modifications : Date / Auteur / Raison
+*
+* Compilateur : Visual Studio 2015
+*/
+# include <stdio.h>
+# include <stdlib.h>
+
+/*----------------------------------------------------------------------------*/
+int main(void)
+{
+
+	/* Introduire ici vos déclarations */
+
+	/* Introduire ici vos instructions */
+	printf("\n\nProgramme ...");
+
+	/* Pour terminer */
+	system("pause");
+	return EXIT_SUCCESS;
+}
